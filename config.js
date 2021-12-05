472d304c214241d9a694481ce7321c23b0bdd670
Greyson Harris <64559895+glewharris@users.noreply.github.com>
2021-12-05 11:55:03 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,215 @@
+var config = {
+    style: 'mapbox://styles/glewharris/ckw2i8x9c0hrs14ldnqm03cqx',
+    accessToken: 'pk.eyJ1IjoiZ2xld2hhcnJpcyIsImEiOiJja3c2emFveTZjYnB4Mm9xMWxjd2h0ODB1In0.vX-xzEsJVyooGMXItH3gIA',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    theme: 'dark',
+    use3dTerrain: false,
+    title: "Get to Know Greyson",
+    subtitle: '',
+    byline: 'Prepared for Mapbox, December 2021',
+    footer: 'Created by Greyson Harris with Mapbox Interactive Storytelling',
+    chapters: [
+        {
+            id: 'intro',
+            alignment: 'left',
+            hidden: false,
+            title: 'A Biography in Seven Places',
+            image: '',
+            description: '',
+            location: {
+                center: [-120.28273, 37.07930],
+                zoom: 5,
+                pitch: 35,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'hyampom',
+            alignment: 'right',
+            hidden: false,
+            title: 'Hyampom, California',
+            image: '',
+            description: '',
+            location: {
+                center: [-122.90946, 40.49466],
+                zoom: 9,
+                pitch: 60,
+                bearing: -62.40
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'italy-1',
+            alignment: 'right',
+            hidden: false,
+            title: 'Italy',
+            image: '',
+            description: '2004-2005 | Study abroad',
+            location: {
+                center: [15.25179, 40.85754],
+                zoom: 6,
+                pitch: 60,
+                bearing: -10
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'italy-2',
+            alignment: 'right',
+            hidden: false,
+            title: 'Omegna, Italy',
+            image: '',
+            description: '',
+            location: {
+                center: [8.63161, 45.89058],
+                zoom: 9.5,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'davis',
+            alignment: 'right',
+            hidden: false,
+            title: 'University of California, Davis',
+            image: '',
+            description: '2006-2010',
+            location: {
+                center: [-121.58357, 38.55520],
+                zoom: 11,
+                pitch: 22,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'california',
+            alignment: 'right',
+            hidden: false,
+            title: 'California',
+            image: '',
+            description: '2010-2013 | Archaeology, US Forest Service',
+            location: {
+                center: [-121.48435, 39.33791],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'spain',
+            alignment: 'right',
+            hidden: false,
+            title: 'Spain',
+            image: '',
+            description: '2011-2012 | ESL, Ministry of Education',
+            location: {
+                center: [-3.81085, 39.94177],
+                zoom: 9,
+                pitch: 59.5,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'guatemala',
+            alignment: 'right',
+            hidden: false,
+            title: 'GW and Antigua, Guatemala',
+            image: '',
+            description: "2013-2015 | Anthropology, GIS, and 'La Roya'",
+            location: {
+                center: [-90.49789, 14.57151],
+                zoom: 8,
+                pitch: 60,
+                bearing: 20
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'dc',
+            alignment: 'right',
+            hidden: false,
+            title: 'Washington, DC',
+            image: '',
+            description: 'Until 2020 | Smithsonian and Esri',
+            location: {
+                center: [-77.02372, 38.89194],
+                zoom: 12,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'placerville',
+            alignment: 'right',
+            hidden: false,
+            title: 'Placerville, California',
+            image: '',
+            description: "Present Day",
+            location: {
+                center: [-120.94874, 38.71138],
+                zoom: 10,
+                pitch: 65,
+                bearing: -99
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
